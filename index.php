482c96ad1e8515f42551f4fcbb3e7e11551621e1
rahmatmulyadifadillah <rahmatmulyadifadillah@gmail.com>
2026-04-29 10:23:12 +0800

    Delete index.php

--- index.php
+++
@@ -1,3 +0,0 @@
-<br><br><a href="materi1.php"></button> Materi 1 : If dan Else</button></a>
-<br><br><a href="materi2.php"></button> Materi 2 : Looping </button></a>
-br><br><a href="materi3.php"></button> Materi 3 : function </button></a>
