cddf6a69d0b294740d9e26ef6d5469bc06bf29f9
rahmatmulyadifadillah <rahmatmulyadifadillah@gmail.com>
2026-04-29 10:00:43 +0800

    Upload Pertama

---
+++ index.php
@@ -0,0 +1,4 @@
+<br><br><a href="materi1.php"></button> Materi 1 : If dan Else</button></a>
+<br><br><a href="materi2.php"></button> Materi 2 : Looping</button></a>
+<br><br><a href="materi3.php"></button> Materi 3 : function</button></a>
+<br><br><a href="materi4.php"></button> Materi 4 : login</button></a>
